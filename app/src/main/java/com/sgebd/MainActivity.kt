ee1652d0f430516716e7d37373614f552b52c5d7
alex98102011 <alex98102011@hotmail.com>
2021-06-08 20:48:30 -0500

    Menu y activity_main.xml

--- app/src/main/java/com/sgebd/MainActivity.kt
+++ app/src/main/java/com/sgebd/MainActivity.kt
@@ -2,10 +2,25 @@ package com.sgebd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    lateinit var editNoControl : EditText
+    lateinit var editPass : EditText
+    lateinit var btnAcceder : Button
+    lateinit var btnRegistrar : Button
+    lateinit var btnInscripcion: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNoControl = findViewById<EditText>(R.id.editNoControl)
+        editPass = findViewById<EditText>(R.id.editContrasenia)
+        btnAcceder = findViewById<Button>(R.id.btnAcceder)
+        btnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+
+
+
     }
 }
---
+++ app/src/main/java/com/sgebd/MenuActivity.kt
@@ -0,0 +1,26 @@
+package com.sgebd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MenuActivity : AppCompatActivity() {
+    lateinit var btnKardex : Button
+    lateinit var btnHorario : Button
+    lateinit var btnReticula : Button
+    lateinit var btnPersonales : Button
+    lateinit var btnInscripcion: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+        btnKardex = findViewById(R.id.btnKardex)
+        btnHorario = findViewById(R.id.btnHorario)
+        btnReticula = findViewById(R.id.btnReticula)
+        btnPersonales = findViewById(R.id.btnPersonales)
+        btnInscripcion=findViewById(R.id.btnInscripcion)
+
+    }
+}
