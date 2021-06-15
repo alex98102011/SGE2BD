b2e83275b4918ebb128fda7a95e4a2a01086d329
alex98102011 <alex98102011@hotmail.com>
2021-06-14 19:34:57 -0500

    Implementacion de los miembros de la clase SQLite

--- app/src/main/java/com/sgebd/models/DBManager.kt
+++ app/src/main/java/com/sgebd/models/DBManager.kt
@@ -1,5 +1,19 @@
 package com.sgebd.models
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DBManager (): SQLiteOpenHelper()
+class DBManager (context: Context?,
+                 name: String?,
+                 factory: SQLiteDatabase.CursorFactory?,
+                 version: Int): SQLiteOpenHelper(context, name, factory, version)
+{
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
